a15768513f0255f6353af00e338f75b5f21729cb
Ryan <ryanmahara@gmail.com>
2022-06-07 12:35:05 -0500

    add basic data structure for muscle groups

---
+++ data.js
@@ -0,0 +1,75 @@
+const muscles = {
+  Neck: {
+    Sternocleidomastoid: 0,
+    Splenius: 0,
+  },
+  Shoulders: {
+    Deltoid: {
+      Anterior: 0,
+      Lateral: 0,
+      Posterior: 0,
+    },
+    Supraspinatus: 0,
+  },
+  'Upper Arms': {
+    'Triceps Brachii': 0,
+    'Biceps Brachii': 0,
+    Brachialis: 0,
+  },
+  Forearms: {
+    Brachioradialis: 0,
+    Wrist: {
+      Flexors: 0,
+      Extensors: 0,
+    },
+    Pronators: 0,
+    Supinators: 0,
+  },
+  back: {
+    general: 0,
+    'Latissimus Dorsi & Teres Major': 0,
+    trapezius: {
+      upper: 0,
+      middle: 0,
+      lower: 0,
+    },
+    'Levator Scapulae': 0,
+    Rhomboids: 0,
+    'Infraspinatus & Teres Minor': 0,
+    Subscapularis: 0,
+  },
+  chest: {
+    general: 0,
+    'Pectoralis Major': {
+      Sternal: 0,
+      Clavicular: 0,
+    },
+    'Pectoralis Minor': 0,
+    'Serratus Anterior': 0,
+  },
+  waist: {
+    'Rectus Abdominis': 0,
+    'Transverse Abdominis': 0,
+    Obliques: 0,
+    'Quadratus Lumborum': 0,
+    'Erector Spinae': 0,
+  },
+  hips: {
+    'Gluteus Maximus': 0,
+    Abductors: 0,
+    Flexors: 0,
+    'Deep External Rotators': 0,
+  },
+  thighs: {
+    Quadriceps: 0,
+    Hamstrings: 0,
+    'Hip Adductors': 0,
+  },
+  Calves: {
+    general: 0,
+    Gastrocnemius: 0,
+    Soleus: 0,
+    'Tibialis Anterior': 0,
+    Popliteus: 0,
+  },
+};
